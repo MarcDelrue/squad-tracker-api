a83938073eeb2ba300e1d76d708d615c05aaa2f3
m.delrue <m.delrue@criteo.com>
2022-06-21 19:44:07 +0200

    Set Redis to comment to be setup later

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,21 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
-import {MicroserviceOptions, Transport} from "@nestjs/microservices";
 
 
 async function bootstrap() {
 
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-      AppModule,
-      {
-        transport: Transport.REDIS,
-        options: {
-          url: 'redis://delrue:9Pw7YS@M2w4f5JV@redis-18853.c3.eu-west-1-2.ec2.cloud.redislabs.com:18853',
-        }
-      }
-  );
-  await app.listen();
+  // const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+  //     AppModule,
+  //     {
+  //       transport: Transport.REDIS,
+  //       options: {
+  //         url: 'redis://delrue:9Pw7YS@M2w4f5JV@redis-18853.c3.eu-west-1-2.ec2.cloud.redislabs.com:18853',
+  //       }
+  //     }
+  // );
+  // await app.listen();
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
 }
 
 bootstrap();
